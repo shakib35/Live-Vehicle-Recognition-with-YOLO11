2cf15fc44613d5738ad8d8c11df730e151eebb48
shakib35 <squddus@students.kennesaw.edu>
2024-10-27 14:30:47 -0400

    add mps and cuda to test

--- YoLo Test/Ultralytics Test.py
+++ YoLo Test/Ultralytics Test.py
@@ -6,7 +6,12 @@ import torch
 model = YOLO("yolo11n.pt")
 
 # Check if MPS is available, otherwise fallback to CPU
-device = "mps" if torch.backends.mps.is_available() else "cpu"
+if torch.backends.mps.is_available:
+    device = "mps"
+elif torch.cuda.is_available:
+    device = 'cuda'
+else:
+    device = 'cpu'
 
 
 results_train = model.train(data="coco8.yaml", epochs=10, imgsz=640, device=device)
