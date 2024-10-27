e386d8ef9a5c5816e29b076a8ca473aed1b4a874
shakib35 <squddus@students.kennesaw.edu>
2024-10-27 15:06:04 -0400

    fix test for cuda and cpu

--- YoLo Test/Ultralytics Test.py
+++ YoLo Test/Ultralytics Test.py
@@ -4,15 +4,12 @@ import torch
 
 # Load a COCO-pretrained YOLO11n model
 model = YOLO("yolo11n.pt")
-
 # Check if MPS is available, otherwise fallback to CPU
-if torch.backends.mps.is_available:
-    device = "mps"
-elif torch.cuda.is_available:
-    device = 'cuda'
-else:
-    device = 'cpu'
 
+if torch.backends.mps.is_available:
+    device = "cuda"
+else:
+    device = "cpu"
 
 results_train = model.train(data="coco8.yaml", epochs=10, imgsz=640, device=device)
 
