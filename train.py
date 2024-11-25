e3f5d4e249c0fc238e600f5575148f4bd1e1710d
shakib35 <squddus@students.kennesaw.edu>
2024-11-25 01:19:29 -0500

    CPU run complete

--- train.py
+++ train.py
@@ -1,36 +1,55 @@
+import os
 from ultralytics import YOLO
+import comet_ml
 from comet_ml import start
 import torch
 import time
 
+# Comet environment variables
+os.environ["COMET_EVAL_BATCH_LOGGING_INTERVAL"] = "5"
+os.environ["COMET_EVAL_LOG_CONFUSION_MATRIX"] = "false"
+
+COMET_SUPPORTED_TASKS = ["detect"]
+EVALUATION_PLOT_NAMES = "F1_curve", "P_curve", "R_curve", "PR_curve", "confusion_matrix"
+LABEL_PLOT_NAMES = "labels", "labels_correlogram"
 
 # Comet logging setup
 experiment = start(
-    api_key = "W2y4B8fSkwtOQPElr8nni3A1H",
-    project_name = "YoLov11_Car_Object_Detection",
+    api_key="W2y4B8fSkwtOQPElr8nni3A1H",
+    project_name="YoLov11_Car_Object_Detection",
     workspace="shakib35"
 )
+experiment_name = "GPU test"
+experiment.set_name(experiment_name)
+
 # Parameters
-device = "mps"
+device = "gpu"
 model = YOLO("yolo11n.pt")
 
 # Logging Parameters to Comet
 experiment.log_parameter("device", device)
 experiment.log_parameter("epochs", 10)
 
-
 start_time = time.time()
-# train model based on yaml
-results = model.train(data="config.yaml", epochs=10, device=device)
 
-# print run time
+# Train model based on YAML configuration
+results = model.train(
+    data="config.yaml", 
+    project="YoLov11_Car_Object_Detection",
+    save_period=1,
+    batch=64,
+    epochs=10,
+    device=device
+)
+
+# Print runtime
 end_time = time.time()
 training_time = end_time - start_time
 print(f"Training completed in {training_time} seconds")
-experiment.log_metric("training_time ", training_time)
+experiment.log_metric(name="training_time", value=training_time)
 
 # Save final trained model
-model_path = 'Car_Object_Detection_mps'
+model_path = 'Car_Object_Detection_cpu'
 model.save(model_path)
 print(f"Model saved to {model_path}")
 experiment.log_model("trained_model", model_path)
@@ -38,19 +57,8 @@ experiment.log_model("trained_model", model_path)
 # Evaluate on validation data to calculate metrics
 metrics = model.val()
 
-# Log evaluation metrics to Comet
-experiment.log_metric("precision", metrics.box.precision.mean())
-experiment.log_metric("recall", metrics.box.recall.mean())
-experiment.log_metric("mAP_50", metrics.box.map50)
-experiment.log_metric("mAP_50_95", metrics.box.map)
+# Log validation metrics
 
-# Log a Precision-Confidence Curve to Comet
-precision_confidence_curve = metrics.box.pr_curve  # Assuming this is supported by YOLO
-experiment.log_curve("Precision-Confidence Curve", precision_confidence_curve.confidence, precision_confidence_curve.precision)
-
-# Log Confusion Matrix to Comet
-confusion_matrix = metrics.box.confusion_matrix
-experiment.log_confusion_matrix(matrix=confusion_matrix, labels=metrics.names)
 
 # End the experiment
 experiment.end()
