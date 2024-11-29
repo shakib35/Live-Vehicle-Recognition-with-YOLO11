7826f4e5c78c7b3d73a2b2f3694a27b5a8424d17
shakib35 <squddus@students.kennesaw.edu>
2024-11-29 04:23:42 -0500

    small edits for paper

--- YoLo Test/tracking test.py
+++ YoLo Test/tracking test.py
@@ -2,9 +2,9 @@ from ultralytics import YOLO
 import cv2
 
 # Path to the YOLO model and video file
-model_path = r"B:\Documents\GitHub\Live-Vehicle-Recognition-with-YOLO11\Car Detection\medium\weights\best.pt"
+model_path = r'B:\Documents\GitHub\Live-Vehicle-Recognition-with-YOLO11\Old Image Classificaiton Model\YoLov11_Car_Object_Detection\train8\weights\best.pt'
 video_path = r"E:\vehicles.mp4"
 # Load the YOLO model
 model = YOLO(model_path)
 
-results = model.track(video_path, show=True, tracker='bytetrack.yaml')
+results = model.predict(video_path, show=True, tracker='bytetrack.yaml')
